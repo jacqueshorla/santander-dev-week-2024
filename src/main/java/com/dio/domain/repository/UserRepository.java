a2d98a0babdc8cc513d24d20f52602b9670d37d5
andarino <tj.vinci97@gmail.com>
2024-08-19 16:33:38 -0300

    Initial commit

---
+++ src/main/java/com/dio/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.dio.domain.repository;
+
+
+import com.dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
